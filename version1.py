812dfbb6f607027feac009703045087ab3e86a83
Boris Reyes <38927619+borisreyesgrange@users.noreply.github.com>
2018-05-03 08:12:07 -0300

    Create version1.py

---
+++ version1.py
@@ -0,0 +1,77 @@
+p3, p5, p7 = 100, 120, 200
+ram16, ram32 = 75, 150
+sto1tb, sto2tb = 50, 100
+sc19, sc23 = 65, 120
+mini, midi = 40, 70
+usb2, usb4 = 10, 20
+
+userProcessor = userRam = userSto = userScreen = userCase = userUsb = estimateNumber = totalPrice = componentIndex = 0
+chosenProcessor = chosenRam = chosenSto = chosenScreen = chosenCase = chosenUsb = ""
+
+print("Choose your Components, enter the option number 1, 2 or 3: ")
+
+while userProcessor not in ("1", "2", "3"):
+    userProcessor= input("Processors: \n1)p3 = $100 \n2)p5 = $120 \n3)p7 = $200\n")
+while userRam not in ("1", "2"):
+    userRam = input("RAM Memories: \n1)16GB = $75 \n2)32GB = $150\n")
+while userSto not in ("1", "2"):
+    userSto = input("Storage: \n1)1TB = $50 \n2)2TB = $100\n")
+while userScreen not in ("1", "2"):
+    userScreen = input("Screen: \n1)19' = $65 \n2)23' = $120\n")
+while userCase not in ("1", "2"):
+    userCase = input("Case: \n1)Mini Tower = $40 \n2)Midi Tower = $70\n")
+while userUsb not in ("1", "2"):
+    userUsb = input("USB: \n1)2 ports = $10 \n2)4 ports = $20\n")
+
+componentsChosen = [userProcessor,userRam,userSto,userCase,userScreen,userUsb]
+
+for component in componentsChosen:
+    if(component == "1"):
+        if (componentIndex == 0):
+            totalPrice += p3
+            chosenProcessor = "PROCESSOR:\t p3  \t $100\n"
+        if (componentIndex == 1):
+            totalPrice += ram16
+            chosenRam = "RAM MEMORY:\t 16 GB\t $75\n"
+        if (componentIndex == 2):
+            totalPrice += sto1tb
+            chosenSto = "STORAGE:\t 1 TB\t $50\n"
+        if (componentIndex == 3):
+            totalPrice += sc19
+            chosenScreen = "SCREEN:  \t 19' \t $65\n"
+        if (componentIndex == 4):
+            totalPrice += mini
+            chosenCase = "CASE:   \t mini T\t $40\n"
+        if (componentIndex == 5):
+            totalPrice += usb2
+            chosenUsb = "USB:    \t 2ports\t $10\n"
+        componentIndex +=1
+    elif (component =="2"):
+        if (componentIndex == 0):
+            totalPrice += p5
+            chosenProcessor = "PROCESSOR:\t p5  \t $120\n"
+        if (componentIndex == 1):
+            totalPrice += ram32
+            chosenRam = "RAM MEMORY:\t 32 GB\t $150\n"
+        if (componentIndex == 2):
+            totalPrice += sto2tb
+            chosenSto = "STORAGE:\t 2 TB\t $100\n"
+        if (componentIndex == 3):
+            totalPrice += sc23
+            chosenScreen = "SCREEN:  \t 23' \t $120\n"
+        if (componentIndex == 4):
+            totalPrice += midi
+            chosenCase = "CASE:   \t midi T\t $70\n"
+        if (componentIndex == 5):
+            totalPrice += usb4
+            chosenUsb = "USB:    \t 4ports\t $20\n"
+        componentIndex += 1
+    elif (component =="3"):
+        totalPrice += p7
+        chosenProcessor = "PROCESSOR:\t p7  \t $200\n"
+        componentIndex += 1
+
+print("*** ESTIMATE NUMBER: "+str(estimateNumber+1)+" ***")
+print("COMPONENT\t OPTION\t PRICE")
+print(str(chosenProcessor)+str(chosenRam)+str(chosenSto)+str(chosenScreen)+str(chosenCase)+str(chosenUsb)+"\n TOTAL PRICE: $"+str(totalPrice*1.2))
+estimateNumber+=1
