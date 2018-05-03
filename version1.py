b085912dd34f139d263216d7347b4cf4eed16fd9
Boris Reyes <38927619+borisreyesgrange@users.noreply.github.com>
2018-05-03 13:04:06 -0300

    Update version1.py

--- version1.py
+++ version1.py
@@ -6,12 +6,13 @@ mini, midi = 40, 70
 usb2, usb4 = 10, 20
 
 userProcessor = userRam = userSto = userScreen = userCase = userUsb = estimateNumber = totalPrice = componentIndex = 0
-chosenProcessor = chosenRam = chosenSto = chosenScreen = chosenCase = chosenUsb = ""
+estimateComponents = ""
 
+print("PRE-RELEASE 2018 - TASK 1\n")
 print("Choose your Components, enter the option number 1, 2 or 3: ")
 
 while userProcessor not in ("1", "2", "3"):
-    userProcessor= input("Processors: \n1)p3 = $100 \n2)p5 = $120 \n3)p7 = $200\n")
+    userProcessor = input("Processors: \n1)p3 = $100 \n2)p5 = $120 \n3)p7 = $200\n")
 while userRam not in ("1", "2"):
     userRam = input("RAM Memories: \n1)16GB = $75 \n2)32GB = $150\n")
 while userSto not in ("1", "2"):
@@ -23,55 +24,87 @@ while userCase not in ("1", "2"):
 while userUsb not in ("1", "2"):
     userUsb = input("USB: \n1)2 ports = $10 \n2)4 ports = $20\n")
 
-componentsChosen = [userProcessor,userRam,userSto,userCase,userScreen,userUsb]
+componentsChosen = [userProcessor, userRam, userSto, userCase, userScreen, userUsb]
 
 for component in componentsChosen:
-    if(component == "1"):
-        if (componentIndex == 0):
+    if component == "1":
+        if componentIndex == 0:
             totalPrice += p3
-            chosenProcessor = "PROCESSOR:\t p3  \t $100\n"
-        if (componentIndex == 1):
+            estimateComponents += "PROCESSOR:\t p3  \t $100\n"
+            componentsChosen[componentIndex] = "p3"
+        if componentIndex == 1:
             totalPrice += ram16
-            chosenRam = "RAM MEMORY:\t 16 GB\t $75\n"
-        if (componentIndex == 2):
+            estimateComponents += "RAM MEMORY:\t 16 GB\t $75\n"
+            componentsChosen[componentIndex] = "ram16"
+        if componentIndex == 2:
             totalPrice += sto1tb
-            chosenSto = "STORAGE:\t 1 TB\t $50\n"
-        if (componentIndex == 3):
+            estimateComponents += "STORAGE:\t 1 TB\t $50\n"
+            componentsChosen[componentIndex] = "sto1tb"
+        if componentIndex == 3:
             totalPrice += sc19
-            chosenScreen = "SCREEN:  \t 19' \t $65\n"
-        if (componentIndex == 4):
+            estimateComponents += "SCREEN:  \t 19' \t $65\n"
+            componentsChosen[componentIndex] = "sc19"
+        if componentIndex == 4:
             totalPrice += mini
-            chosenCase = "CASE:   \t mini T\t $40\n"
-        if (componentIndex == 5):
+            estimateComponents += "CASE:   \t mini T\t $40\n"
+            componentsChosen[componentIndex] = "mini"
+        if componentIndex == 5:
             totalPrice += usb2
-            chosenUsb = "USB:    \t 2ports\t $10\n"
-        componentIndex +=1
-    elif (component =="2"):
-        if (componentIndex == 0):
-            totalPrice += p5
-            chosenProcessor = "PROCESSOR:\t p5  \t $120\n"
-        if (componentIndex == 1):
-            totalPrice += ram32
-            chosenRam = "RAM MEMORY:\t 32 GB\t $150\n"
-        if (componentIndex == 2):
-            totalPrice += sto2tb
-            chosenSto = "STORAGE:\t 2 TB\t $100\n"
-        if (componentIndex == 3):
-            totalPrice += sc23
-            chosenScreen = "SCREEN:  \t 23' \t $120\n"
-        if (componentIndex == 4):
-            totalPrice += midi
-            chosenCase = "CASE:   \t midi T\t $70\n"
-        if (componentIndex == 5):
-            totalPrice += usb4
-            chosenUsb = "USB:    \t 4ports\t $20\n"
+            estimateComponents += "USB:    \t 2ports\t $10\n"
+            componentsChosen[componentIndex] = "usb2"
         componentIndex += 1
-    elif (component =="3"):
+    elif component == "2":
+        if componentIndex == 0:
+            totalPrice += p5
+            estimateComponents += "PROCESSOR:\t p5  \t $120\n"
+            componentsChosen[componentIndex] = "p5"
+        if componentIndex == 1:
+            totalPrice += ram32
+            estimateComponents += "RAM MEMORY:\t 32 GB\t $150\n"
+            componentsChosen[componentIndex] = "ram32"
+        if componentIndex == 2:
+            totalPrice += sto2tb
+            estimateComponents += "STORAGE:\t 2 TB\t $100\n"
+            componentsChosen[componentIndex] = "sto2tb"
+        if componentIndex == 3:
+            totalPrice += sc23
+            estimateComponents += "SCREEN:  \t 23' \t $120\n"
+            componentsChosen[componentIndex] = "sc23"
+        if componentIndex == 4:
+            totalPrice += midi
+            estimateComponents += "CASE:   \t midi T\t $70\n"
+            componentsChosen[componentIndex] = "midi"
+        if componentIndex == 5:
+            totalPrice += usb4
+            estimateComponents += "USB:    \t 4ports\t $20\n"
+            componentsChosen[componentIndex] = "usb4"
+        componentIndex += 1
+    elif component == "3":
         totalPrice += p7
-        chosenProcessor = "PROCESSOR:\t p7  \t $200\n"
+        estimateComponents += "PROCESSOR:\t p7  \t $200\n"
+        componentsChosen[componentIndex] = "p7"
         componentIndex += 1
 
 print("*** ESTIMATE NUMBER: "+str(estimateNumber+1)+" ***")
 print("COMPONENT\t OPTION\t PRICE")
-print(str(chosenProcessor)+str(chosenRam)+str(chosenSto)+str(chosenScreen)+str(chosenCase)+str(chosenUsb)+"\n TOTAL PRICE: $"+str(totalPrice*1.2))
-estimateNumber+=1
+print(estimateComponents+"\nTOTAL COST: $"+str(totalPrice*1.2)+"\n**************************\n")
+print("PRE-RELEASE 2018 - TASK 2\n")
+
+p3Stock = p5Stock = p7Stock = ram16Stock = ram32Stock = sto1tbStock = sto2tbStock = sc19Stock = sc23Stock = miniStock = midiStock = usb2Stock = usb4Stock = 10
+stockCheck = 0
+ordersNumbers = list()
+
+print("*** STOCK CHECK ***")
+for component in componentsChosen:
+    if eval(component+"Stock") > 0:
+        print(str(component)+" ..... OK")
+        stockCheck += 1
+    else:
+        print(str(component) + " ..... NO STOCK")
+if stockCheck == 6:
+    print("\n*** STOCK UPDATE ***")
+    for component in componentsChosen:
+        exec(str(component+"Stock -= 1"))
+        print(str(component)+" -> "+str(eval(component+"Stock")))
+
+#ordersNumbers.append(estimateNumber)
